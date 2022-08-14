cd3003061ba5dd45357c5411f7f04d1159f3deae
Vusal Gurbanov <98140799+Vusal-Gurbanov@users.noreply.github.com>
2022-08-14 09:27:57 +0300

    Add files via upload

---
+++ KDVhesaplama.java
@@ -0,0 +1,24 @@
+ikinci odevim
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class KDVhesaplama {
+    public static void main(String[] args) {
+         double urunTutari, kdvOran = 0.18, kdvTutar, kdvliTutar;
+
+         Scanner input = new Scanner(System.in);
+         System.out.print("ürün tutarını giriniz: ");
+         urunTutari = input.nextDouble();
+
+         kdvTutar = urunTutari * kdvOran;
+         kdvliTutar = urunTutari + kdvTutar;
+
+        System.out.println("KDV siz tutar:" + urunTutari);
+        System.out.println("KDV oranı: " + kdvOran);
+        System.out.println("KDV Tutarı: " + kdvTutar);
+        System.out.println("KDV' li tutar : " + kdvliTutar);
+
+
+
+
+    }
+}
